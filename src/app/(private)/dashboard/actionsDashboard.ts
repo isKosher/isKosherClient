95413060a16eece4793a382cc5a42f4452efb2b8
Daniel-Nashnaz <daniel.81953@gmail.com>
2025-02-18 22:03:09 +0200

    [IMPROVE] Add actions server

--- src/app/(private)/dashboard/actionsDashboard.ts
+++
@@ -1,9 +0,0 @@
-import { RestaurantPreview } from "@/types";
-import { apiFetch } from "@/utils/axiosConfig";
-
-export async function getMyRestaurants(): Promise<RestaurantPreview[]> {
-  return apiFetch<RestaurantPreview[]>("/admin/businesses/my-businesses", {
-    method: "GET",
-    includeCookies: true,
-  });
-}
