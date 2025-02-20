6ff9e26d5d261ac87bf8683d6cd8d0d5983ed6d1
Daniel-Nashnaz <daniel.81953@gmail.com>
2025-02-20 21:59:18 +0200

    [MINOR] Add use in .env file

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,2 +1,2 @@
-export const BASE_URL_IS_KOSHER_MANAGER = "https://iskoshermanager.onrender.com/api/v1";
+export const BASE_URL_IS_KOSHER_MANAGER = process.env.IS_KOSHER_MANAGER_URL;
 export const AXIOS_DEFAULT_TIMEOUT = 60000; // 60 seconds (1 minute)
