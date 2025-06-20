aa2d3bc02247051d4c879dde98b161c673ac640e
Daniel-Nashnaz <daniel.81953@gmail.com>
2025-06-20 16:55:42 +0300

    [IMPROVE] Displays nearby businesses on details page
    Fetches and displays nearby businesses on the business details page, enhancing user experience by providing quick access to related options.
    
    This change replaces the previous implementation, which fetched nearby businesses within the component, with a server-side approach. This improves performance and SEO, as the data is now pre-rendered.
    
    It also modifies the way the direction apps are opened.

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -4,3 +4,8 @@ export const UPLOAD_DELAY = 1000; // 1 second
 
 export const AVATAR_SUPERVISOR_URL = "https://lh3.googleusercontent.com/d/1mBGdKNPdaelz6mgm6SBFUXzefP16mrWn=w1000";
 export const REVALIDATE_TIME = 60 * 60; // 1 hour in seconds
+export const TIMEOUT_MS = 120000; // 120 seconds (2 minutes)
+
+export const RADIUS_NEARBY_BUSINESS = 5;
+export const PAGE_NEARBY_BUSINESS = 1;
+export const SIZE_NEARBY_BUSINESS = 6;
